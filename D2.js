f22a576b3d59dc8a98a90f130609d4996e437905
scialuppa2 <scialuppa2@gmail.com>
2023-10-31 16:47:51 +0100

    esercizio 31/10/2023

---
+++ D2.js
@@ -0,0 +1,189 @@
+/* ESERCIZIO 1
+ Scrivi un algoritmo per trovare il più grande tra due numeri interi.
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+/*const num1 = parseInt(prompt("Inserisci il primo numero"))
+const num2 = parseInt(prompt("Inserisci il secondo numero"))
+
+let numeroPiuGrande
+
+if(num1 >= num2){
+  numeroPiuGrande = num1
+}
+else{
+  numeroPiuGrande = num2
+}
+alert(`il numero più grande è ${numeroPiuGrande} `)*/
+
+/* ESERCIZIO 2
+  Scrivi un algoritmo che mostri "not equal" in console se un numero intero fornito è diverso da 5.
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+/*const numero = parseInt(prompt("Inserisci un numero"))
+
+if(numero != 5){
+  console.log("not equal")
+}
+else{
+  console.log("equal")
+}*/
+
+/* ESERCIZIO 3
+  Scrivi un algoritmo che mostri "divisibile per 5" in console se un numero fornito è perfettamente divisibile per 5 (suggerimento: cerca l'operatore modulo su un motore di ricerca)
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+/*const div5 = parseInt(prompt("Inserisci un numero"))
+
+if(div5 %5===0){
+  console.log("divisibile per 5")
+}
+else{
+  console.log("non divisibile per 5")
+}*/
+
+/* ESERCIZIO 4
+  Scrivi un algoritmo per verificare che, dati due numeri interi, il valore di uno di essi sia 8 oppure se la loro addizione/sottrazione sia uguale a 8.
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+/*const numero1 = parseInt(prompt("Inserisci il primo numero"))
+const numero2 = parseInt(prompt("Inserisci il secondo numero"))
+
+if(numero1 === 8 || numero2 === 8){
+  console.log("valore di 8")
+}
+else if(numero1 + numero2 === 8 || numero1 - numero2 === 8 || numero2 - numero1 === 8){
+  console.log("valore di 8")
+}
+else{
+  console.log("valore differente da 8")
+}*/
+
+/* ESERCIZIO 5
+  Stai lavorando su un sito di e-commerce. Stai salvando il saldo totale del carrello dell'utente in una variabile "totalShoppingCart".
+  C'è una promozione in corso: se il totale del carrello supera 50, l'utente ha diritto alla spedizione gratuita (altrimenti la spedizione ha un costo fisso pari a 10).
+  Crea un algoritmo che determini l'ammontare totale che deve essere addebitato all'utente per il checkout.
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+/*const acquisto1 = parseInt(prompt("Primo acquisto"))
+const acquisto2 = parseInt(prompt("Secondo acquisto"))
+let totalShoppingCart = acquisto1 + acquisto2
+
+if(totalShoppingCart > 50){
+  alert(`Totale: ${totalShoppingCart} Complimenti !!! Spedizione gratuita`)
+}
+else{
+  alert(`Totale: ${totalShoppingCart} Costo spedizione: 10`)
+}*/
+
+
+/* ESERCIZIO 6
+  Stai lavorando su un sito di e-commerce. Oggi è il Black Friday e viene applicato il 20% su ogni prodotto.
+  Modifica la risposta precedente includendo questa nuova promozione nell'algoritmo, determinando come prima se le spedizioni sono gratuite oppure no e e calcolando il totale.
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+/*const acquisto1 = parseInt(prompt("Primo acquisto"))
+const acquisto2 = parseInt(prompt("Secondo acquisto"))
+let totalShoppingCart = ((acquisto1 - (acquisto1*20)/100)) + ((acquisto2 - (acquisto2+20)/100))
+
+if(totalShoppingCart > 50){
+  alert(`Totale: ${totalShoppingCart} Complimenti !!! Spedizione gratuita`)
+}
+else{
+  alert(`Totale: ${totalShoppingCart} Costo spedizione: 10`)
+}*/
+
+/* ESERCIZIO 7
+  Crea tre variabili, e assegna un valore numerico a ciascuna di esse.
+  Utilizzando un blocco condizionale, crea un algoritmo per ordinarle secondo il loro valore, dal più alto al più basso.
+  Alla fine mostra il risultato in console.
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+/*let numeri = [24, 36, 12]
+numeri.sort()
+console.log(numeri)*/
+
+/* ESERCIZIO 8
+  Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+/*const codice = prompt("Inserisci un codice")
+
+if(typeof codice === Number ){
+  console.log("numero")
+}
+else{
+  console.log("altro")
+}*/
+
+
+/* ESERCIZIO 9
+  Crea un algoritmo per controllare se un numero fornito sia pari o dispari (suggerimento: cerca l'operatore modulo su un motore di ricerca)
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+/*const numeroFornito = parseInt(prompt("Inserisci un numero"))
+
+if(numeroFornito %2 === 0){
+  alert(`${numeroFornito} è pari`)
+}
+else{
+  alert(`${numeroFornito} è dispari`)
+}*/
+
+/* ESERCIZIO 10
+  Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.
+  */
+  /* SCRIVI QUI LA TUA RISPOSTA */
+  /*let val = 7
+  if (val < 10) {
+      console.log("Meno di 10");
+    } else if (val > 5) {
+      console.log("Maggiore di 5");
+    } else {
+      console.log("Uguale a 10 o maggiore");
+    }*/
+
+/* ESERCIZIO 11
+  Fornito il seguente oggetto, scrivi del codice per aggiungere una proprietà "city", il cui valore sarà "Toronto".
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+/*const me = {
+  name: 'John',
+  lastName: 'Doe',
+  skills: ['javascript', 'html', 'css'],
+}
+me.city = 'Toronto'
+console.log(me)*/
+
+/* ESERCIZIO 12
+  Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere la proprietà "lastName".
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+/*const me = {
+  name: 'John',
+  lastName: 'Doe',
+  skills: ['javascript', 'html', 'css'],
+}
+delete me.lastName
+console.log(me)*/
+
+/* ESERCIZIO 13
+  Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere l'ultimo elemento della proprietà "skills".
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+/*const me = {
+  name: 'John',
+  lastName: 'Doe',
+  skills: ['javascript', 'html', 'css'],
+}
+delete me.skills['css']
+console.log(me)*/
+
+/* ESERCIZIO 14
+  Scrivi del codice per creare un array inizialmente vuoto. Riempilo successivamente con i numeri da 1 a 10.
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 15
+  Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
